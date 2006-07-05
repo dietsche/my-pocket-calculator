4bbead43a5e6b45aed05c14f6f8186644a29ee06
Tabish Fayyaz Mufti <tabishfayyaz@gmail.com>
2006-07-05 10:15:17 +0000

    XML File Structure totally changed. BaseUnit tag added to xml file xml file converted only base to subunit ratios

--- MyPocketCal2003/Class Files/UnitConversion.cs
+++ MyPocketCal2003/Class Files/UnitConversion.cs
@@ -16,6 +16,51 @@ namespace MyPocketCal2003
             //get the respective quantity node
             XmlNode quantityNode = docXmlFile.SelectSingleNode("/Quantities/Quantity[Name='" + quantityName + "']");
    
+            //select the baseUnit node
+            XmlNode node = quantityNode.SelectSingleNode("BaseUnit");
+            //the name of the baseUnit
+            String baseUnit = node.InnerText;
+            
+            //inputUnit -> baseUnit -> outputUnit:
+
+            //converting from inputUnit to baseUnit....baseUnit act as intermediary
+            XmlNode conversionNode;
+            double inputToBaseRatio = 1.0;
+
+            //there is no ratio available for going from A unit to A unit i.e. inputUnit=baseUnit otherwise:
+            if (!inputUnit.Equals(baseUnit)) 
+            {
+                //finding the ratio of baseUnit-->inputUnit then getting its inverse to form inputUnit-->baseUnit
+                conversionNode = quantityNode.SelectSingleNode("Conversion[ToUnit='" + inputUnit + "']");   
+                inputToBaseRatio = 1.0/Convert.ToDouble(conversionNode.LastChild.InnerText); //inputUnit to baseUnit conversion ratio
+            }
+            
+            double baseToOutputRatio = 1.0;
+
+            //there is no ratio available for going from A unit to A unit i.e. baseUnit=outputUnit otherwise:
+            if (!outputUnit.Equals(baseUnit)) 
+            {
+                //converting from baseUnit (intermediary) to outputUnit
+                conversionNode = quantityNode.SelectSingleNode("Conversion[ToUnit='" + outputUnit + "']");
+                baseToOutputRatio = Convert.ToDouble(conversionNode.LastChild.InnerText); //baseUnit to outputUnit conversion ratio
+            }
+
+            //converting input to base unit then converting base unit to output unit
+            return Convert.ToString((Convert.ToDouble(input)*inputToBaseRatio)*baseToOutputRatio);
+        }
+    }
+}
+
+
+
+/*public String convert(String input, String quantityName, String inputUnit, String outputUnit, XmlDocument docXmlFile)
+        {
+            if(inputUnit.Equals(outputUnit)) //if same unit conversion
+                return Convert.ToString(input);
+
+            //get the respective quantity node
+            XmlNode quantityNode = docXmlFile.SelectSingleNode("/Quantities/Quantity[Name='" + quantityName + "']");
+   
             //get the first conversion node in the quantity
             XmlNode node = quantityNode.SelectSingleNode("Conversion[1]");
 
@@ -29,15 +74,18 @@ namespace MyPocketCal2003
             XmlNode conversionNode;
             double inputToBaseRatio = 1.0;
 
-            if (!inputUnit.Equals(baseUnit)) //there is no ratio available for going from A unit to A unit i.e. inputUnit=baseUnit
+            //there is no ratio available for going from A unit to A unit i.e. inputUnit=baseUnit
+            if (!inputUnit.Equals(baseUnit)) 
             {
-                conversionNode = quantityNode.SelectSingleNode("Conversion[FromUnit='" + inputUnit + "' and ToUnit='" + baseUnit + "']");
-                inputToBaseRatio = Convert.ToDouble(conversionNode.LastChild.InnerText); //inputUnit to baseUnit conversion ratio
+                //finding the ratio of baseUnit-->inputUnit then getting its inverse to form inputUnit-->baseUnit
+                conversionNode = quantityNode.SelectSingleNode("Conversion[FromUnit='" + baseUnit + "' and ToUnit='" + inputUnit + "']");   
+                inputToBaseRatio = 1.0/Convert.ToDouble(conversionNode.LastChild.InnerText); //inputUnit to baseUnit conversion ratio
             }
-
+            
             double baseToOutputRatio = 1.0;
 
-            if (!outputUnit.Equals(baseUnit)) //there is no ratio available for going from A unit to A unit i.e. baseUnit=outputUnit
+            //there is no ratio available for going from A unit to A unit i.e. baseUnit=outputUnit
+            if (!outputUnit.Equals(baseUnit)) 
             {
                 //converting from baseUnit (intermediary) to outputUnit
                 conversionNode = quantityNode.SelectSingleNode("Conversion[FromUnit='" + baseUnit + "' and ToUnit='" + outputUnit + "']");
@@ -47,5 +95,4 @@ namespace MyPocketCal2003
             //converting input to base unit then converting base unit to output unit
             return Convert.ToString((Convert.ToDouble(input)*inputToBaseRatio)*baseToOutputRatio);
         }
-    }
-}
+   */
